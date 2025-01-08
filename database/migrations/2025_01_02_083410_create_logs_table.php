16a81b83ab34128a356856f7ecfd71c8929cb174
Nikhil <nikhil@gmail.com>
2025-01-08 14:12:12 +0530

    Cron for translations

--- database/migrations/2025_01_02_083410_create_logs_table.php
+++ database/migrations/2025_01_02_083410_create_logs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('file_name')->nullable();
             $table->text('message')->nullable();
             $table->json('payload')->nullable();
