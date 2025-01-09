9ceee16d9d7fae7eeea1ec2ebdcbbd7910664d96
shivanshi111 <shivanshi@example.com>
2025-01-09 13:56:11 +0530

    Home page language show and changed the URL

--- database/migrations/2024_11_06_134410_create_languages_table.php
+++ database/migrations/2024_11_06_134410_create_languages_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -24,6 +25,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;'); // Disable foreign key checks
         Schema::dropIfExists('languages');
-    }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;'); 
+        }
 };
