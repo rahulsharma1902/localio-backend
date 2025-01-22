759998993b862248cf6988f46bdf5ed88f699962
shivanshi111 <shivanshi@example.com>
2025-01-22 15:30:33 +0530

    migration of trails table

---
+++ database/migrations/2025_01_22_095540_create_trails_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trails', function (Blueprint $table) {
+            $table->id(); // Primary Key
+            $table->unsignedBigInteger('product_id'); // Foreign Key to Products table
+            $table->integer('free_trial_days'); // Free trial days
+            $table->timestamps(); // created_at and updated_at
+
+            // Foreign Key Constraint (Optional, if you have a products table)
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trails');
+    }
+};
--- resources/views/admin_layout/master.blade.php
+++ resources/views/admin_layout/master.blade.php
@@ -227,40 +227,6 @@
                                                 </li>
                                             </ul>
                                         </li>
-                                         <li class="nk-menu-item has-sub">
-                                    <a href="#" class="nk-menu-link nk-menu-toggle">
-                                        <span class="nk-menu-icon"><em class="icon ni ni-setting-fill"></em></span>
-                                        <span class="nk-menu-text">Setting</span>
-                                    </a>
-                                    <ul class="nk-menu-sub">
-                                        <li class="nk-menu-item">
-                                            <a href="{{ url('/admin-dashboard/site-languages') }}"
-                                                class="nk-menu-link"><span class="nk-menu-text">Site -
-                                                    Languages</span></a>
-                                        </li>
-                                        <li class="nk-menu-item">
-                                            <a href="{{ url('/admin-dashboard/country') }}" class="nk-menu-link"><span
-                                                    class="nk-menu-text">Country</span></a>
-                                        </li>
-
-                                    </ul>
-                                </li>
-                                        
-
-                                        <!-- <li class="nk-menu-item has-sub">
-                                            <a href="#" class="nk-menu-link nk-menu-toggle">
-                                                <span class="nk-menu-icon"><em class="icon ni ni-article"></em></span>
-                                                <span class="nk-menu-text">Reviews Section</span>
-                                            </a>
-                                            <ul class="nk-menu-sub">
-                                                <li class="nk-menu-item">
-                                                    <a href="{{ url('/admin-dashboard/reviews') }}"
-                                                        class="nk-menu-link"><span class="nk-menu-text">Reviews</span>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </li> -->
-
                                     </ul>
                                 </li>
                                 <li class="nk-menu-item has-sub">
