bd4880e58244cd4eb45ab8d386286eedb30618ea
Nikhil <nikhil@gmail.com>
2025-01-22 13:19:24 +0530

    Removed the too many request error

--- app/Http/Middleware/AddLocaleAutomatically.php
+++ app/Http/Middleware/AddLocaleAutomatically.php
@@ -7,20 +7,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
-use App\Models\SiteLanguages;
 use Session;
 use Illuminate\Support\Facades\Route;
 class AddLocaleAutomatically
 {
     public function handle(Request $request, Closure $next)
     {
-        $languages = getLanguages(true);
-        $currentRoute = $request->route();
+        // $languages = getLanguages(true);
+        // $currentRoute = $request->route();
         $locale = $request->route('locale');
-        if (!$locale || !in_array($locale, $languages)) {
-            return redirect()->route($currentRoute->getName(), ['locale' => 'en-us']);
-        }
-        app()->setLocale($locale);
-        return $next($request);
+        // if (!$locale || !in_array($locale, $languages)) {
+        //     return redirect()->route('home', ['locale' => 'en-us']);
+        // }
+        
+        // else{
+            app()->setLocale($locale);
+            return $next($request);
+        // // }
+    
     }
 }
